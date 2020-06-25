2397a042ab418853f48521285ee9069708e0b95c
Adam Corbin <acorbin3@gmail.com>
2020-06-25 08:26:40 -0400

    changes to get the searching working

--- app/src/main/java/com/example/podplay/viewmodel/SearchViewModel.kt
+++ app/src/main/java/com/example/podplay/viewmodel/SearchViewModel.kt
@@ -4,38 +4,44 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import com.example.podplay.repository.ItunesRepo
 import com.example.podplay.service.PodcastResponse
+import util.DateUtils
+
 class SearchViewModel(application: Application) :
     AndroidViewModel(application) {
-    fun searchPodcasts(term: String, function: (Nothing) -> Unit) {
+    var iTunesRepo: ItunesRepo? = null
 
+    data class PodcastSummaryViewData(
+        var name: String? = "",
+        var lastUpdated: String? = "",
+        var imageUrl: String? = "",
+        var feedUrl: String? = ""
+    )
+
+    private fun itunesPodcastToPodcastSummaryView(
+        itunesPodcast: PodcastResponse.ItunesPodcast
+    ):
+            PodcastSummaryViewData {
+        return PodcastSummaryViewData(
+            itunesPodcast.collectionCensoredName,
+            DateUtils.jsonDateToShortDate(itunesPodcast.releaseDate),
+            itunesPodcast.artworkUrl30,
+            itunesPodcast.feedUrl
+        )
     }
-}
 
-var iTunesRepo: ItunesRepo? = null
-
-data class PodcastSummaryViewData(
-    var name: String? = "",
-    var lastUpdated: String? = "",
-    var imageUrl: String? = "",
-    var feedUrl: String? = "")
-
-private fun itunesPodcastToPodcastSummaryView(
-    itunesPodcast: PodcastResponse.ItunesPodcast):
-        PodcastSummaryViewData {
-    return PodcastSummaryViewData(
-        itunesPodcast.collectionCensoredName,
-        itunesPodcast.releaseDate,
-        itunesPodcast.artworkUrl30,
-        itunesPodcast.feedUrl)
-}
-
-fun searchPodcasts(term: String,    callback: (List<PodcastSummaryViewData>) -> Unit) {
-    iTunesRepo?.searchByTerm(term) { results ->
-        if (results == null) {
-            callback(emptyList())
-        } else {
-      val searchViews = results.map { podcast ->    itunesPodcastToPodcastSummaryView(podcast)    }
-            callback(searchViews)
+    fun searchPodcasts(term: String, callback: (List<PodcastSummaryViewData>) -> Unit) {
+        iTunesRepo?.searchByTerm(term) { results ->
+            if (results == null) {
+                callback(emptyList())
+            } else {
+                val searchViews =
+                    results.map { podcast -> itunesPodcastToPodcastSummaryView(podcast) }
+                callback(searchViews)
+            }
         }
     }
 }
+
+
+
+
