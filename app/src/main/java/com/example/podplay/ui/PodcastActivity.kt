9086f061dc084000ffbec69c0510c816f8d39171
Jenay <jenaywalker@aol.com>
2020-06-25 04:28:23 -0400

    initial commit

--- app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
@@ -2,17 +2,62 @@ package com.example.podplay.ui
 
 import android.app.SearchManager
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.widget.SearchView
+import android.view.View
+import androidx.activity.viewModels
+import androidx.appcompat.widget.SearchView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.podplay.R
+import com.example.podplay.adapter.PodcastListAdapter
 import com.example.podplay.repository.ItunesRepo
 import kotlinx.android.synthetic.main.activity_podcast.*
 import com.example.podplay.service.ItunesService
+import com.example.podplay.viewmodel.PodcastSummaryViewData
+import com.example.podplay.viewmodel.SearchViewModel
 
-class PodcastActivity : AppCompatActivity() {
+
+class PodcastActivity : AppCompatActivity(),
+    PodcastListAdapter.PodcastListAdapterListener {
+    override fun onShowDetails(
+        podcastSummaryViewData: PodcastSummaryViewData) {}
+        private fun showProgressBar() {
+            progressBar.visibility = View.VISIBLE
+        }
+        private fun hideProgressBar() {
+            progressBar.visibility = View.INVISIBLE
+        }
+
+
+
+
+
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
+    private fun setupToolbar() {   setSupportActionBar(toolbar) }
+
+    private fun setupViewModels() {
+        val service = ItunesService.instance
+        //searchViewModel.iTunesRepo = ItunesRepo(service)
+    }
+
+    private fun updateControls() {   podcastRecyclerView.setHasFixedSize(true)
+        val layoutManager = LinearLayoutManager(this)
+        podcastRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            podcastRecyclerView.context, layoutManager.orientation)
+        podcastRecyclerView.addItemDecoration(dividerItemDecoration)
+        podcastListAdapter = PodcastListAdapter(null,
+            this, this)
+        podcastRecyclerView.adapter = podcastListAdapter
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_podcast)
@@ -20,8 +65,12 @@ class PodcastActivity : AppCompatActivity() {
         val TAG = javaClass.simpleName
         val itunesService = ItunesService.instance
         val itunesRepo = ItunesRepo(itunesService)
-        itunesRepo.searchByTerm("Android Developer") {   Log.i(TAG, "Results = $it") }
+        itunesRepo.searchByTerm("") {   Log.i(TAG, "Results = $it") }
+        setupToolbar()
+        setupViewModels()
+        updateControls()
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_search, menu)
@@ -31,6 +80,30 @@ class PodcastActivity : AppCompatActivity() {
                 as SearchManager
         searchView.setSearchableInfo(
                 searchManager.getSearchableInfo(componentName))
-        return true }
+        return true
+    }
+    private fun performSearch(term: String) {
+        showProgressBar()
+        searchViewModel.searchPodcasts(term) {results ->
+            hideProgressBar()
+            toolbar.title = getString(R.string.search_hint)
+            podcastListAdapter.setSearchData(results)
+        }
+
+
+
+    }
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?:
+            return
+            performSearch(query)
+        }
+    }
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
 
 }
