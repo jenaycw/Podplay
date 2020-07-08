d5fb863c4fcd20b984f560a60d790dcdb5d337fa
Jenay <jenaywalker@aol.com>
2020-07-08 09:22:38 -0400

    initial commit

--- app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
+++ app/src/main/java/com/example/podplay/ui/PodcastActivity.kt
@@ -1,40 +1,114 @@
 package com.example.podplay.ui
 
-import android.app.AlertDialog
+
 import android.app.SearchManager
 import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings.Global.getString
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.appcompat.widget.SearchView
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.activity.viewModels
-import androidx.appcompat.widget.SearchView
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.work.*
 import com.example.podplay.R
 import com.example.podplay.adapter.PodcastListAdapter
+import com.example.podplay.adapter.PodcastListAdapter.PodcastListAdapterListener
 import com.example.podplay.db.PodPlayDatabase
 import com.example.podplay.repository.ItunesRepo
 import com.example.podplay.repository.PodcastRepo
+import com.example.podplay.worker.EpisodeUpdateWorker
 import com.example.podplay.service.FeedService
-import kotlinx.android.synthetic.main.activity_podcast.*
 import com.example.podplay.service.ItunesService
+import com.example.podplay.ui.PodcastDetailsFragment.OnPodcastDetailsListener
 import com.example.podplay.viewmodel.PodcastViewModel
 import com.example.podplay.viewmodel.SearchViewModel
+import kotlinx.android.synthetic.main.activity_podcast.*
+import java.util.concurrent.TimeUnit
 
+class PodcastActivity : AppCompatActivity(), PodcastListAdapterListener,
+    OnPodcastDetailsListener {
 
-class PodcastActivity : AppCompatActivity(),
-    PodcastListAdapter.PodcastListAdapterListener {
+    private val searchViewModel by viewModels<SearchViewModel>()
+    private val podcastViewModel by viewModels<PodcastViewModel>()
+    private lateinit var podcastListAdapter: PodcastListAdapter
+    private lateinit var searchMenuItem: MenuItem
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_podcast)
+        setupToolbar()
+        setupViewModels()
+        updateControls()
+        setupPodcastListView()
+        handleIntent(intent)
+        addBackStackListener()
+        scheduleJobs()
+    }
+
+    override fun onSubscribe() {
+        podcastViewModel.saveActivePodcast()
+        supportFragmentManager.popBackStack()
+    }
+
+    override fun onUnsubscribe() {
+        podcastViewModel.deleteActivePodcast()
+        supportFragmentManager.popBackStack()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_search, menu)
+
+        searchMenuItem = menu.findItem(R.id.search_item)
+        val searchView = searchMenuItem.actionView as SearchView
+
+        searchMenuItem.setOnActionExpandListener(object: MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionExpand(p0: MenuItem?): Boolean {
+                return true
+            }
+            override fun onMenuItemActionCollapse(p0: MenuItem?): Boolean {
+                showSubscribedPodcasts()
+                return true
+            }
+        })
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        if (supportFragmentManager.backStackEntryCount > 0) {
+            podcastRecyclerView.visibility = View.INVISIBLE
+        }
+
+        if (podcastRecyclerView.visibility == View.INVISIBLE) {
+            searchMenuItem.isVisible = false
+        }
+
+        return true
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent)
+    }
+
 
     override fun onShowDetails(podcastSummaryViewData: SearchViewModel.PodcastSummaryViewData) {
+
         val feedUrl = podcastSummaryViewData.feedUrl ?: return
+
         showProgressBar()
+
         podcastViewModel.getPodcast(podcastSummaryViewData) {
+
             hideProgressBar()
+
             if (it != null) {
                 showDetailsFragment()
             } else {
@@ -43,17 +117,55 @@ class PodcastActivity : AppCompatActivity(),
         }
     }
 
-    private fun showProgressBar() {
-        progressBar.visibility = View.VISIBLE
+    private fun scheduleJobs() {
+
+        val constraints: Constraints = Constraints.Builder().apply {
+            setRequiredNetworkType(NetworkType.CONNECTED)
+            setRequiresCharging(true)
+        }.build()
+
+        val request = PeriodicWorkRequestBuilder<EpisodeUpdateWorker>(
+            1, TimeUnit.HOURS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(this).enqueueUniquePeriodicWork(TAG_EPISODE_UPDATE_JOB,
+            ExistingPeriodicWorkPolicy.REPLACE, request)
     }
 
-    private fun hideProgressBar() {
-        progressBar.visibility = View.INVISIBLE
+    private fun showSubscribedPodcasts()
+    {
+        val podcasts = podcastViewModel.getPodcasts()?.value
+
+        if (podcasts != null) {
+            toolbar.title = getString(R.string.subscribed_podcasts)
+            podcastListAdapter.setSearchData(podcasts)
+        }
+    }
+
+    private fun performSearch(term: String) {
+        showProgressBar()
+        searchViewModel.searchPodcasts(term) { results ->
+            hideProgressBar()
+            toolbar.title = term
+            podcastListAdapter.setSearchData(results)
+        }
+    }
+
+    private fun handleIntent(intent: Intent) {
+        if (Intent.ACTION_SEARCH == intent.action) {
+            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
+            performSearch(query)
+        }
+        val podcastFeedUrl = intent.getStringExtra(EpisodeUpdateWorker.EXTRA_FEED_URL)
+        if (podcastFeedUrl != null) {
+            podcastViewModel.setActivePodcast(podcastFeedUrl) {
+                it?.let { podcastSummaryView -> onShowDetails(podcastSummaryView) }
+            }
+        }
     }
 
 
-    private val searchViewModel by viewModels<SearchViewModel>()
-    private lateinit var podcastListAdapter: PodcastListAdapter
     private fun setupToolbar() {
         setSupportActionBar(toolbar)
     }
@@ -67,110 +179,65 @@ class PodcastActivity : AppCompatActivity(),
         podcastViewModel.podcastRepo = PodcastRepo(rssService, podcastDao)
     }
 
+    private fun setupPodcastListView() {
+        podcastViewModel.getPodcasts()?.observe(this, Observer {
+            if (it != null) {
+                showSubscribedPodcasts()
+            }
+        })
+    }
+
+    private fun addBackStackListener()
+    {
+        supportFragmentManager.addOnBackStackChangedListener {
+            if (supportFragmentManager.backStackEntryCount == 0) {
+                podcastRecyclerView.visibility = View.VISIBLE
+            }
+        }
+    }
+
     private fun updateControls() {
         podcastRecyclerView.setHasFixedSize(true)
+
         val layoutManager = LinearLayoutManager(this)
         podcastRecyclerView.layoutManager = layoutManager
 
         val dividerItemDecoration = DividerItemDecoration(
-            podcastRecyclerView.context, layoutManager.orientation
-        )
+            podcastRecyclerView.context, layoutManager.orientation)
         podcastRecyclerView.addItemDecoration(dividerItemDecoration)
-        podcastListAdapter = PodcastListAdapter(
-            null,
-            this, this
-        )
+
+        podcastListAdapter = PodcastListAdapter(null, this, this)
         podcastRecyclerView.adapter = podcastListAdapter
     }
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_podcast)
-
-        val TAG = javaClass.simpleName
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo(itunesService)
-
-        setupToolbar()
-        setupViewModels()
-        updateControls()
-        addBackStackListener()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        val inflater = menuInflater
-        inflater.inflate(R.menu.menu_search, menu)
-        searchMenuItem = menu.findItem(R.id.search_item)
-        val searchView = searchMenuItem.actionView as SearchView
-        val searchManager = getSystemService(Context.SEARCH_SERVICE)
-                as SearchManager
-        searchView.setSearchableInfo(
-            searchManager.getSearchableInfo(componentName)
-        )
-        if (supportFragmentManager.backStackEntryCount > 0) {
-            podcastRecyclerView.visibility = View.INVISIBLE }
-        if (podcastRecyclerView.visibility == View.INVISIBLE) {
-            searchMenuItem.isVisible = false
-        }
-        return true
-    }
-
-    private fun performSearch(term: String) {
-        val itunesService = ItunesService.instance
-        val itunesRepo = ItunesRepo(itunesService)
-        itunesRepo.searchByTerm(term) { Log.i("TAG", "Results = $it") }
-        showProgressBar()
-        searchViewModel.searchPodcasts(term) { results ->
-            hideProgressBar()
-            toolbar.title = term
-            podcastListAdapter.setSearchData(results)
-        }
-
-
-    }
-
-    private fun handleIntent(intent: Intent) {
-        if (Intent.ACTION_SEARCH == intent.action) {
-            val query = intent.getStringExtra(SearchManager.QUERY) ?: return
-            performSearch(query)
-        }
-    }
-
-    override fun onNewIntent(intent: Intent) {
-        super.onNewIntent(intent)
-        setIntent(intent)
-        handleIntent(intent)
-    }
-
-    companion object {
-        private const val TAG_DETAILS_FRAGMENT = "DetailsFragment"
-    }
-
-    private lateinit var searchMenuItem: MenuItem
-    private fun createPodcastDetailsFragment():
-            PodcastDetailsFragment {
-        var podcastDetailsFragment = supportFragmentManager
-            .findFragmentByTag(TAG_DETAILS_FRAGMENT) as
-                PodcastDetailsFragment?
-        if (podcastDetailsFragment == null) {
-            podcastDetailsFragment =
-                PodcastDetailsFragment.newInstance()
-        }
-        return podcastDetailsFragment
-    }
-
     private fun showDetailsFragment() {
         val podcastDetailsFragment = createPodcastDetailsFragment()
-        supportFragmentManager.beginTransaction().add(
-            R.id.podcastDetailsContainer,
-            podcastDetailsFragment, TAG_DETAILS_FRAGMENT
-        )
-            .addToBackStack("DetailsFragment").commit()
+
+        supportFragmentManager.beginTransaction().add(R.id.podcastDetailsContainer,
+            podcastDetailsFragment, TAG_DETAILS_FRAGMENT).addToBackStack("DetailsFragment").commit()
         podcastRecyclerView.visibility = View.INVISIBLE
         searchMenuItem.isVisible = false
     }
 
+    private fun createPodcastDetailsFragment(): PodcastDetailsFragment {
+        var podcastDetailsFragment = supportFragmentManager.findFragmentByTag(TAG_DETAILS_FRAGMENT) as
+                PodcastDetailsFragment?
+
+        if (podcastDetailsFragment == null) {
+            podcastDetailsFragment = PodcastDetailsFragment.newInstance()
+        }
+
+        return podcastDetailsFragment
+    }
+
+    private fun showProgressBar() {
+        progressBar.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressBar() {
+        progressBar.visibility = View.INVISIBLE
+    }
 
     private fun showError(message: String) {
         AlertDialog.Builder(this)
@@ -180,13 +247,8 @@ class PodcastActivity : AppCompatActivity(),
             .show()
     }
 
-    private val podcastViewModel by viewModels<PodcastViewModel>()
-    private fun addBackStackListener() {
-        supportFragmentManager.addOnBackStackChangedListener {
-            if (supportFragmentManager.backStackEntryCount == 0) {
-                podcastRecyclerView.visibility = View.VISIBLE     }
-        }
+    companion object {
+        private const val TAG_DETAILS_FRAGMENT = "DetailsFragment"
+        private const val TAG_EPISODE_UPDATE_JOB = "com.raywenderlich.podplay.episodes"
     }
-
-
 }
--- app/src/main/java/com/example/podplay/ui/PodcastDetailsFragment.kt
+++ app/src/main/java/com/example/podplay/ui/PodcastDetailsFragment.kt
@@ -1,190 +1,126 @@
 package com.example.podplay.ui
-
-import android.content.ComponentName
-import android.net.Uri
+import android.content.Context
 import android.os.Bundle
-import android.support.v4.media.MediaBrowserCompat
-import android.support.v4.media.MediaMetadataCompat
-import android.support.v4.media.session.MediaControllerCompat
-import android.support.v4.media.session.MediaSessionCompat
-import android.support.v4.media.session.PlaybackStateCompat
 import android.text.method.ScrollingMovementMethod
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.podplay.R
 import com.example.podplay.adapter.EpisodeListAdapter
-import com.example.podplay.adapter.EpisodeListAdapterListener
-import com.example.podplay.service.PodplayMediaService
 import com.example.podplay.viewmodel.PodcastViewModel
 import kotlinx.android.synthetic.main.fragment_podcast_details.*
+import android.view.LayoutInflater
+import android.view.MenuInflater
+import android.view.MenuItem
 
-class PodcastDetailsFragment : Fragment(), EpisodeListAdapterListener {
 
-    private lateinit var mediaBrowser: MediaBrowserCompat
-    private var mediaControllerCallback: MediaControllerCallback? = null
+
+
+
+
+class PodcastDetailsFragment : Fragment() {
+
+    private val podcastViewModel: PodcastViewModel by activityViewModels()
     private lateinit var episodeListAdapter: EpisodeListAdapter
+    private var listener: OnPodcastDetailsListener? = null
+    private var menuItem: MenuItem? = null
 
-
+    companion object {
+        fun newInstance(): PodcastDetailsFragment {
+            return PodcastDetailsFragment()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
-        initMediaBrowser()
     }
 
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?, savedInstanceState: Bundle?
-    ):
-            View? {
-        return inflater.inflate(
-            R.layout.fragment_podcast_details,
-            container, false
-        )
-
-
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_podcast_details, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
-
         super.onActivityCreated(savedInstanceState)
+        setupControls()
+        updateControls()
     }
 
-    override fun onCreateOptionsMenu(
-        menu: Menu,
-        inflater: MenuInflater)
-    {
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         super.onCreateOptionsMenu(menu, inflater)
         inflater.inflate(R.menu.menu_details, menu)
-        updateControls()
-
-
+        menuItem = menu.findItem(R.id.menu_feed_action)
+        updateMenuItem()
     }
 
-    private val podcastViewModel: PodcastViewModel by activityViewModels()
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_feed_action -> {
+                podcastViewModel.activePodcastViewData?.feedUrl?.let {
+
+                    if (podcastViewModel.activePodcastViewData?.subscribed == true) {
+                        listener?.onUnsubscribe()
+                    } else {
+                        listener?.onSubscribe()
+                    }
+                }
+                return true
+            }
+            else ->
+                return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is OnPodcastDetailsListener) {
+            listener = context
+        } else {
+            throw RuntimeException(context.toString() + " must implement OnPodcastDetailsListener")
+        }
+    }
+
+
+
+    private fun setupControls() {
+
+        feedDescTextView.movementMethod = ScrollingMovementMethod()
+
+        episodeRecyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(activity)
+        episodeRecyclerView.layoutManager = layoutManager
+
+        val dividerItemDecoration = DividerItemDecoration(
+            episodeRecyclerView.context, layoutManager.orientation)
+        episodeRecyclerView.addItemDecoration(dividerItemDecoration)
+
+        episodeListAdapter = EpisodeListAdapter(podcastViewModel.activePodcastViewData?.episodes)
+        episodeRecyclerView.adapter = episodeListAdapter
+    }
+
     private fun updateControls() {
         val viewData = podcastViewModel.activePodcastViewData ?: return
         feedTitleTextView.text = viewData.feedTitle
         feedDescTextView.text = viewData.feedDesc
         activity?.let { activity ->
-            Glide.with(activity).load(viewData.imageUrl)
-                .into(feedImageView)
+            Glide.with(activity).load(viewData.imageUrl).into(feedImageView)
         }
-
     }
 
-    companion object {
-        fun newInstance(): PodcastDetailsFragment {
-
-            return PodcastDetailsFragment()
-        }
-    }
-    inner class MediaControllerCallback:
-        MediaControllerCompat.Callback() {
-        override fun onMetadataChanged(metadata:
-                                       MediaMetadataCompat?) {
-            super.onMetadataChanged(metadata)
-            println(
-                "metadata changed to ${metadata?.getString(
-                    MediaMetadataCompat.METADATA_KEY_MEDIA_URI)}")
-        }      override fun onPlaybackStateChanged(state: PlaybackStateCompat?)
-        {
-            super.onPlaybackStateChanged(state)
-            println("state changed to $state")   }
-    }
-    private fun registerMediaController(token: MediaSessionCompat.Token) {
-        val fragmentActivity = activity as FragmentActivity
-        val mediaController = MediaControllerCompat(fragmentActivity, token)
-        MediaControllerCompat.setMediaController(fragmentActivity, mediaController)
-        mediaControllerCallback = MediaControllerCallback()
-        mediaController.registerCallback(mediaControllerCallback!!)
-    }
-    inner class MediaBrowserCallBacks:   MediaBrowserCompat.ConnectionCallback() {
-        override fun onConnected()
-        {
-            super.onConnected()
-            registerMediaController(mediaBrowser.sessionToken)
-            println("onConnected")   }
-        override fun onConnectionSuspended() {
-            super.onConnectionSuspended()
-            println("onConnectionSuspended")
-        }
-        override fun onConnectionFailed() {
-
-        }
-    }
-    private fun initMediaBrowser() {
-        val fragmentActivity = activity as FragmentActivity
-        mediaBrowser = MediaBrowserCompat(fragmentActivity,
-            ComponentName(fragmentActivity,
-                PodplayMediaService::class.java),
-            MediaBrowserCallBacks(),
-            null)
-    }
-    override fun onStart() {
-        super.onStart()
-        if (mediaBrowser.isConnected) {
-            val fragmentActivity = activity as FragmentActivity
-            if (MediaControllerCompat.getMediaController
-                    (fragmentActivity) == null) {
-                registerMediaController(mediaBrowser.sessionToken)
-            }
-        }
-        else {
-            mediaBrowser.connect()
-        }
-    }
-    override fun onStop() {
-        super.onStop()
-        val fragmentActivity = activity as FragmentActivity
-        if (MediaControllerCompat.getMediaController(fragmentActivity)
-            != null)
-        {
-            mediaControllerCallback?.let {
-                MediaControllerCompat.getMediaController(fragmentActivity)
-                    .unregisterCallback(it)
-            }
-        }
-    }
-    private fun startPlaying(
-        episodeViewData: PodcastViewModel.EpisodeViewData) {
-        val fragmentActivity = activity as FragmentActivity
-        val controller =
-            MediaControllerCompat.getMediaController(fragmentActivity)
-        val viewData = podcastViewModel.activePodcastViewData ?:
-        return
-        val bundle = Bundle()
-        bundle.putString(MediaMetadataCompat.METADATA_KEY_TITLE,
-            episodeViewData.title)
-        bundle.putString(MediaMetadataCompat.METADATA_KEY_ARTIST,
-            viewData.feedTitle)
-        bundle.putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI,
-            viewData.imageUrl)
-        controller.transportControls.playFromUri(
-            Uri.parse(episodeViewData.mediaUrl), bundle)
-    }
-    override fun onSelectedEpisode(episodeViewData: PodcastViewModel.EpisodeViewData) {
-        val fragmentActivity = activity as FragmentActivity
-        val controller =
-            MediaControllerCompat.getMediaController(fragmentActivity)
-        if (controller.playbackState != null) {
-            if (controller.playbackState.state ==
-                PlaybackStateCompat.STATE_PLAYING) {
-                controller.transportControls.pause()
-            } else {
-                startPlaying(episodeViewData)
-            }
-        } else {
-            startPlaying(episodeViewData)
-        }
-
+    private fun updateMenuItem() {
+        val viewData = podcastViewModel.activePodcastViewData ?: return
+        menuItem?.title = if (viewData.subscribed) getString(R.string.unsubscribe)
+        else getString(R.string.subscribe)
     }
 
+    interface OnPodcastDetailsListener {
+        fun onSubscribe()
+        fun onUnsubscribe()
+    }
 
 }
